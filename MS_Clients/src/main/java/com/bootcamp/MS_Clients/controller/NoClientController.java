e37785dcb9fa277f94687745f0ee593ae0219a50
ManuelVargas13 <77123147+ManuelVargas13@users.noreply.github.com>
2022-07-22 18:17:45 -0500

    MS_Credits

--- MS_Clients/src/main/java/com/bootcamp/MS_Clients/controller/NoClientController.java
+++ MS_Clients/src/main/java/com/bootcamp/MS_Clients/controller/NoClientController.java
@@ -4,10 +4,12 @@ import com.bootcamp.MS_Clients.model.NoClients;
 import com.bootcamp.MS_Clients.service.ClientService;
 import com.bootcamp.MS_Clients.service.NoClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+@RestController
+@RequestMapping(value="/noClient")
+@CrossOrigin("*")
 public class NoClientController {
 
     @Autowired
